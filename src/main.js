913d9620003b698dc266304b6e8d02a510a4c9ad
chy <1069351592@qq.com>
2020-05-28 17:21:03 +0800

    modified

--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+//import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router';
+//需要加./
+import './assets/fonts/iconfont.css';
+//import './plugins/element.js'
 
+import axios from 'axios';
+import { Button,Input,Form,FormItem,Message,Container,Header,Main,Aside,Menu,Submenu,MenuItem,MenuItemGroup} from 'element-ui'
+
+//须写在前面
 Vue.config.productionTip = false
+
+
+//请求根路径URL需要都大写
+axios.defaults.baseURL="https://www.liulongbin.top:8888/api/private/v1/";
+//配置拦截器
+axios.interceptors.request.use(config=>{
+	//为headers添加一个authorization属性
+	config.headers.Authorization=window.sessionStorage.getItem('token');
+	return config
+})
+//其余组件可使用this.$http
+Vue.prototype.$http=axios;
+
+//Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Aside)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+
+Vue.prototype.$message=Message
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import login from '../components/login.vue'
+import sign from '../components/sign.vue'
+import home from '../components/home.vue'
+import welcome from '../components/welcome.vue'
+import users from '../components/user/users.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+	  //地址的重定向，表示若用户访问的目录为/,则重定向到/login
+	path:'/',  redirect:'/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	  path:'/login',
+	  name:'login',
+	  component:login
+  },
+  {
+	  path:'/sign',
+	  name:'sign',
+	  component:sign
+  },
+  {
+  	  path:'/home',
+  	  name:'home',
+  	  component:home,
+	  redirect:'/welcome',
+	  children:[
+		  {
+			  path:'/welcome',
+			  name:'welcome',
+			  component:welcome
+		  },
+		  {
+		  	 path:'/users',
+		  	 name:'user',
+		  	 component:users
+		  }
+	  ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+//路由守卫函数
+//当没有登录时，访问其它vue跳转到login
+router.beforeEach((to,from,next)=>{
+	//to表示将要访问的路径
+	//from代表从哪个路径跳转而来
+	//next表示是一个函数，表示放行
+	//next()表示直接放行， next("/login")强制跳转到/login
+	if(to.path=='/login') return next();
+	const tokenstr=window.sessionStorage.getItem("token");
+	if(!tokenstr) return next('/login');
+	next();
 })
 
 export default router
